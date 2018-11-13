9b719f8abc80861b1d7c097f5d5714f3e99709bb
Oparin-Salamon Melinda <melinda@oparin.hu>
2018-11-13 18:14:49 +0100

    fix(models): Item, Transactions

--- src/main/java/hu/oparin/bookstore/models/Item.java
+++ src/main/java/hu/oparin/bookstore/models/Item.java
@@ -16,7 +16,63 @@ public class Item {
     private int cost;
     private boolean isSold;
 
+    public Item() {
+    }
 
+    public Item(Long id, Book book, String quality, int price, int cost, boolean isSold) {
+        this.id = id;
+        this.book = book;
+        this.quality = quality;
+        this.price = price;
+        this.cost = cost;
+        this.isSold = isSold;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
 }
--- src/main/java/hu/oparin/bookstore/models/Transaction.java
+++ src/main/java/hu/oparin/bookstore/models/Transaction.java
@@ -9,7 +9,7 @@ public class Transaction {
     @GeneratedValue
     private Long id;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
@@ -18,4 +18,45 @@ public class Transaction {
     private List<Item> items;
 
     private String type;
+
+    public Transaction() {
+    }
+
+    public Transaction(Customer customer, List<Item> items, String type) {
+        this.customer = customer;
+        this.items = items;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
